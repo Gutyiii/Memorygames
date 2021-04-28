68d8e422a890b763a3cbbc67f8f6d7afe08a8b9f
Niko <kissnikolettanett@gmail.com>
2021-04-28 10:12:50 +0200

    jtáéktér/ metodusok generálása

--- public_html/memoryjs.js
+++ public_html/memoryjs.js
@@ -1,37 +1,45 @@
-var kep1={
-    eleresiUt:"kepek/kep1.jpg",
-    alt:"husky"
+var kep1 = {
+    eleresiUt: "kepek/kep1.jpg",
+    alt: "husky",
+    hatlap: "kepek/hatlap2.0.png"
 };
-var kep2={
-    eleresiUt:"kepek/kep2.jpg",
-    alt:"nemetjuhasz"
+var kep2 = {
+    eleresiUt: "kepek/kep2.jpg",
+    alt: "nemetjuhasz",
+    hatlap: "kepek/hatlap2.0.png"
 };
-var kep3={
-    eleresiUt:"kepek/kep3.jpg",
-    alt:"labrador"
+var kep3 = {
+    eleresiUt: "kepek/kep3.jpg",
+    alt: "labrador",
+    hatlap: "kepek/hatlap2.0.png"
 };
-var kep4={
-    eleresiUt:"kepek/kep4.jpg",
-    alt:"bordercollie"
+var kep4 = {
+    eleresiUt: "kepek/kep4.jpg",
+    alt: "bordercollie",
+    hatlap: "kepek/hatlap2.0.png"
 };
-var kep5={
-    eleresiUt:"kepek/kep5.jpg",
-    alt:"goldenretriver"
+var kep5 = {
+    eleresiUt: "kepek/kep5.jpg",
+    alt: "goldenretriver",
+    hatlap: "kepek/hatlap2.0.png"
 };
-var kep6={
-    eleresiUt:"kepek/kep6.jpg",
-    alt:"akita"
+var kep6 = {
+    eleresiUt: "kepek/kep6.jpg",
+    alt: "akita",
+    hatlap: "kepek/hatlap2.0.png"
 };
 
-var hatlap={
-    eleresiUt:"kepek/hatlap2.0.jpg",
-    alt:"hatlap"
+var hatlap = {
+    eleresiUt: "kepek/hatlap2.0.jpg",
+    alt: "hatlap"
 };
 
-kepek = [kep1,kep2,kep3,kep4,kep5,kep6];
+kepek = [kep1, kep2, kep3, kep4, kep5, kep6];
 
 $(function () {
-
+         
+    kezd();
+         
 //    $("div").html("Szia Petra");
 //    $("div").append("<img>");
 //    $("div img").attr("src", "kepek/valami.jpg");
@@ -45,12 +53,29 @@ $(function () {
 //    $("div img").eq(1).attr("alt", kepek[2].alt);
 //    $("div img").click(kattintasra);
     //var i=0;
-    for (var i = 0; i < kepek.length; i++) {
-        $("#jatekter img").eq(i).attr("src", kepek[i].eleresiUt);
-        $("#jatekter img").eq(i).attr("alt", kepek[i].alt);
+    var j=0;
+    for (var i = 0; i < 12; i++) {
+        $("#jatekter img").eq(i).attr("src", kepek[j].hatlap);
+        $("#jatekter img").eq(i).attr("alt", kepek[j].alt);
+        j++;
+        if (j===6){
+            j=0;
+        }
     }
-        
+    
 });
+
+function kezd(){
+    
+}
+
+function ellenoriz(){
+    
+}
+
+function kever(){
+    pontok=0;
+}
 
 //var kep1={
 //    eleresiUt:"kepek/valami.jpg",
@@ -62,7 +87,7 @@ $(function () {
 //};
 
 //var kepek = [kep1,kep2];
-        
+
 
 //function kattintasra() {
 //    $(this).attr("src", "kepek/valami.jpg");
