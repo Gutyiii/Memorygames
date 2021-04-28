66a973c775b69ef9bf1f3673c4827431a71f47ae
Gutyiii <gutyeszbandesz@gmail.com>
2021-04-28 08:42:46 +0200

    alap

---
+++ public_html/memoryjs.js
@@ -0,0 +1,33 @@
+$(function () {
+
+    $("div").html("Szia Petra");
+    $("div").append("<img>");
+//    $("div img").attr("src", "kepek/valami.jpg");
+//    $("div img").attr("alt", "valami");
+    $("div img)".eq(0).attr("src", kepek[0].eleresiUt);
+    $("div img)".eq(0).attr("alt", kepek[0].alt);
+    $("div").append("<img>");
+//    $("div img").eq(1).attr("src", "kepek/valami masik.jpg");
+//    $("div img").eq(1).attr("alt", "sziaaasda13124a");
+    $("div img").eq(1).attr("src", kepek[1].eleresiUt);
+    $("div img").eq(1).attr("alt", kepek[2].alt);
+    $("div img").click(kattintasra);
+
+});
+
+var kep1={
+    eleresiUt:"kepek/valami.jpg",
+    alt:"valami"
+};
+var kep2={
+    eleresiUt:"kepek/valami2.jpg",
+    alt:"sziaaasda13124a"
+};
+
+var kepek = [kep1,kep2];
+        
+
+function kattintasra() {
+    $(this).attr("src", "kepek/valami.jpg");
+    $(this).attr("alt", "valami");
+}
